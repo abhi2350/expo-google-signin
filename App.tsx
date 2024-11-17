31c02e37da585185b2d0bf4ec353cbb54a4b72f0
Credo-Abhishek <abhishek@credo.namstorm.com>
2024-11-17 20:06:10 +0530

    first commit ever

--- App.tsx
+++ App.tsx
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from "expo-web-browser";
+
+interface UserInfo {
+  name: string;
+  email: string;
+  picture: string;
+}
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+  iosClientId: "74629819012-rkiav4q4uv36kerqr43bljf39sk6l96g.apps.googleusercontent.com",
+  clientId: "74629819012-g2h1hmo2gdulnschu2e36ot4ha95b5lh.apps.googleusercontent.com",
+  androidClientId: "74629819012-7pirurm080u9mb9r9mfak69l0cjvge7m.apps.googleusercontent.com",
+});
+
+  useEffect(() => {
+    const fetchUserInfo = async (token: string) => {
+      const userInfoResponse = await fetch(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const user = await userInfoResponse.json();
+      console.log("break 2", user);
+      setUserInfo(user);
+    };
+
+    if (response?.type === "success") {
+      const { access_token } = response.params;
+      fetchUserInfo(access_token);
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {userInfo ? (
+        <View>
+            <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
+          <Text>Name: {userInfo.name}!</Text>
+          <Text>Email: {userInfo.email}</Text>
+        </View>
+      ) : (
+        <Button
+          title="Sign in with Google"
+          onPress={() => {
+            promptAsync({
+              showInRecents: true,
+            });
+          }}
+        />
+      )}
     </View>
   );
 }
@@ -13,8 +64,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profilePic: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    alignSelf: "center"
   },
 });
